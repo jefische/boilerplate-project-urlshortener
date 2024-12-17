db5d30e47a5a443b6774e1c8adf7224fa2c368a0
Jeremy Fischer <jefische86@gmail.com>
2024-12-17 11:20:47 -0500

    challenges complete

--- index.js
+++ index.js
@@ -33,28 +33,63 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.post('/api/shorturl', async function(req, res) {
 	const myUrl = req.body.url;  // using a let instead of var or const causes the value to be erased for some reason.
 
-	// Create a record of a model
-	const urlEntry = new URL({ original_url: myUrl , short_url: 1});
-	// Search for an existing record document
-	const urlSearch = await URL.findOne({ original_url: myUrl}); // we need async function and await for results to return before proceeding with conditional statements.
-	// console.log(urlSearch);
-
-	if (urlSearch) {
-		res.json({ original_url: urlSearch.original_url,
-					short_url: urlSearch.short_url
-		});
+	// (4) If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+	const urlRegex1 = new RegExp(/https:\/\//i);
+	const urlRegex2 = new RegExp(/http:\/\//i);
+	if (!urlRegex1.test(myUrl) && !urlRegex2.test(myUrl)) {
+		res.json({ error: 'invalid url'});
 	} else {
-		urlEntry.save();
-		res.json({ original_url: urlEntry.original_url,
-			short_url: urlEntry.short_url
-		});
+		
+		// Search for an existing record document
+		const urlSearch = await URL.findOne({ original_url: myUrl}); // we need async function and await for results to return before proceeding with conditional statements.
+		// console.log(urlSearch);
+		
+		if (urlSearch) {
+			res.json({ original_url: urlSearch.original_url,
+						short_url: urlSearch.short_url
+					});
+		} else {
+			// Count the number of documents in the DB
+			var count = await URL.countDocuments({});
+			// console.log('document count is: ' + count);
+			
+			// Create a record of our model, and increase the short_url by one.
+			const urlEntry = new URL({ 
+				original_url: myUrl,
+				short_url: count + 1
+			});
+
+			await urlEntry.save();
+			res.json({ original_url: urlEntry.original_url,
+				short_url: urlEntry.short_url
+			});
+		}
 	}
 
 })
 
 // (3) When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+app.get('/api/shorturl/:shorturl', async (req, res) => {
+	var path = req.params.shorturl;
+	const urlSearch = await URL.findOne({ short_url: path });
+	if (urlSearch) {
+		res.redirect(urlSearch.original_url);
+	}
+	else {
+		res.send("short url path not found...");
+	}
+})
 
-// (4) If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+
+
+
+// Delete all records in the DB
+app.get('/api/delete', async (req, res, next) => {
+	var count = await URL.countDocuments({});
+	console.log('deleting ' + count + ' documents...');
+	await URL.deleteMany({});
+	res.redirect('/');
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
